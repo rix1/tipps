18bdbba6a48a6e4c7ff31ffe4f0716c676909c91
rix1 <rikardeide@gmail.com>
2016-05-04 00:48:06 +0200

    finished setting up frontend

--- client/main.js
+++ client/main.js
@@ -3,20 +3,38 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.feedbackForm.onCreated(function helloOnCreated() {
+    // counter starts at 0
+    this.finished = new ReactiveVar(false);
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.feedbackForm.helpers({
+    counter() {
+        return "";
+    },
+
+    finished() {
+        return Template.instance().finished.get();
+    }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.feedbackForm.events({
+    'submit .center-form'(event, instance){
+        event.preventDefault();
+        const target = event.target;
+        let epost = target.emailField.value;
+
+        Meteor.call('emailHandler', epost , (err, res) => {
+              if (err) {
+                  console.log(err.reason);
+              } else {
+                  // $(event.target).text('reset');
+                  console.log(instance.finished.get());
+                  instance.finished.set(!instance.finished.get())
+                  console.log(instance.finished.get());
+                  target.emailField.value = '';
+                  console.log("CLIENT: epost lagret");
+              }
+          });
+    },
 });
--- server/main.js
+++ server/main.js
@@ -3,3 +3,10 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+
+Meteor.methods({
+    emailHandler: (email) => {
+        console.log(`SERVER: received email ${email}`);
+    }
+})
