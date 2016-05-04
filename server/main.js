808e3620cda5546f93050a272cf561e5820a9f08
rix1 <rikardeide@gmail.com>
2016-05-04 06:26:40 +0200

    records are now saved in the database

--- server/main.js
+++ server/main.js
@@ -1,12 +1,21 @@
 import { Meteor } from 'meteor/meteor';
+import { SignUps } from './collections';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    // code to run on server at startup
+    if(SignUps.find().count() === 0){
+        console.log("DATABASE IS EMPTY!");
+    }
 });
 
 
 Meteor.methods({
     emailHandler: (email) => {
         console.log(`SERVER: received email ${email}`);
+        // todo: Do vaalidation!!!
+        SignUps.insert({
+            email,
+            createdAt: new Date(), // current time
+        });
     }
 })
